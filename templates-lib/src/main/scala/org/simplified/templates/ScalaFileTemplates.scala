f66d8768009f740a9181844313c04ffd74f8bf9a
Kostas Kougios <kostas.kougios@googlemail.com>
2023-07-17 14:14:56 +0100

    -

--- templates-lib/src/main/scala/org/simplified/templates/ScalaFileTemplates.scala
+++ templates-lib/src/main/scala/org/simplified/templates/ScalaFileTemplates.scala
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils
 
 class ScalaFileTemplates:
   def apply(code: String, vals: Map[String, Any]): String =
-    val search  = vals.keys.map(k=>s"`$k`").toArray
+    val search  = vals.keys.map(k => s"`$k`").toArray
     val replace = vals.values.toArray.map(_.toString)
     val lines   = StringUtils
       .split(code, '\n')
