096b76ed624bc44edf3de41619843028fdea76e2
Kostas Kougios <kostas.kougios@googlemail.com>
2023-07-17 14:41:21 +0100

    -

--- templates-lib/src/main/scala/org/simplified/templates/ScalaFileTemplates.scala
+++ templates-lib/src/main/scala/org/simplified/templates/ScalaFileTemplates.scala
@@ -4,13 +4,18 @@ import org.apache.commons.lang3.StringUtils
 
 class ScalaFileTemplates:
   def apply(code: String, vals: Product): String =
+    val lines = applyToBlock(StringUtils.split(code, '\n'), vals)
+    lines.mkString("\n")
+
+  private def applyToBlock(lines: Array[String], vals: Product): Array[String] =
+    val (search, replace) = searchReplace(vals)
+    lines.map: line =>
+      StringUtils.replaceEach(line, search, replace)
+
+  private def searchReplace(vals: Product) =
     val search  = vals.productElementNames.map(k => s"`$k`").toArray
     val replace = vals.productIterator.map(_.toString).toArray
-    val lines   = StringUtils
-      .split(code, '\n')
-      .map: line =>
-        StringUtils.replaceEach(line, search, replace)
-    lines.mkString("\n")
+    (search, replace)
 
 object ScalaFileTemplates:
   def apply() = new ScalaFileTemplates
--- templates-lib/src/test/scala/org/simplified/templates/ScalaFileTemplatesTest.scala
+++ templates-lib/src/test/scala/org/simplified/templates/ScalaFileTemplatesTest.scala
@@ -5,15 +5,32 @@ import org.scalatest.matchers.should.Matchers.*
 
 class ScalaFileTemplatesTest extends AnyFunSuiteLike:
   val templates = ScalaFileTemplates()
-  test("replaces vals"):
+  test("replaces vals") {
     case class Vals(x: String, y: String)
     templates(
       """
-         |val `x`=5
-         |val `y`=6
-         |""".stripMargin.trim,
+        |val `x`=5
+        |val `y`=6
+        |""".stripMargin.trim,
       Vals("a", "b")
     ) should be("""
         |val a=5
         |val b=6
         |""".stripMargin.trim)
+  }
+
+  test("// = for") {
+    case class LoopVals(x: String, y: Int)
+    case class Vals(loop: Seq[LoopVals])
+    templates(
+      s"""
+         |// = for loop
+         |val `x`=`y`
+         |// = end loop
+         |""".stripMargin.trim,
+      Vals(Seq(LoopVals("a", 1), LoopVals("b", 2)))
+    ) should be("""
+        |val a=1
+        |val b=2
+        |""".stripMargin.trim)
+  }
