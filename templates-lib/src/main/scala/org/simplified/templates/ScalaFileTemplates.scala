5e375fac3a68bd60e6c80030c381ce49251c5dbc
Kostas Kougios <kostas.kougios@googlemail.com>
2023-07-17 14:49:47 +0100

    -

--- templates-lib/src/main/scala/org/simplified/templates/ScalaFileTemplates.scala
+++ templates-lib/src/main/scala/org/simplified/templates/ScalaFileTemplates.scala
@@ -3,14 +3,17 @@ package org.simplified.templates
 import org.apache.commons.lang3.StringUtils
 
 class ScalaFileTemplates:
+  private val ForEach                            = "// = foreach "
   def apply(code: String, vals: Product): String =
-    val lines = applyToBlock(StringUtils.split(code, '\n'), vals)
+    val (search, replace) = searchReplace(vals)
+    val lines             = applyToBlock(StringUtils.split(code, '\n').toList, search, replace)
     lines.mkString("\n")
 
-  private def applyToBlock(lines: Array[String], vals: Product): Array[String] =
-    val (search, replace) = searchReplace(vals)
-    lines.map: line =>
-      StringUtils.replaceEach(line, search, replace)
+  private def applyToBlock(lines: List[String], search: Array[String], replace: Array[String]): List[String] =
+    lines match
+      case Nil       => Nil
+      case line :: l =>
+        StringUtils.replaceEach(line, search, replace) :: applyToBlock(l, search, replace)
 
   private def searchReplace(vals: Product) =
     val search  = vals.productElementNames.map(k => s"`$k`").toArray
