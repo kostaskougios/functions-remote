aa4a7a44968fa5eb61c7dd3af18344c7f09e94bf
Kostas Kougios <kostas.kougios@googlemail.com>
2023-07-19 15:57:35 +0100

    -

--- macros/src/main/scala/console/macros/model/EMethod.scala
+++ macros/src/main/scala/console/macros/model/EMethod.scala
@@ -3,19 +3,4 @@ package console.macros.model
 import org.simplified.templates.model.{Param, Params}
 
 case class EMethod(name: String, paramss: List[List[EParam]], returnType: EType):
-  def toParams = Params(paramss.flatten.map(ep => Param(ep.name, ep.typeUnqualified)))
-
-  /** @return
-    *   params as code i.e. "(path : String, lsOptions : ls.model.LsOptions)"
-    */
-  def paramsCode: String = paramss.map(_.map(_.code)).map(_.mkString(", ")).mkString("(", "", ")")
-
-  /** @return
-    *   params as code with types unqualified, i.e. "(path:String, lsOptions:LsOptions)"
-    */
-  def paramsCodeUnqualified: String = paramss.map(_.map(p => p.name + ":" + p.typeUnqualified)).map(_.mkString(", ")).mkString("(", "", ")")
-
-  /** @return
-    *   the params as args i.e. "(path, lsOptions)"
-    */
-  def paramsAsArgs: String = paramss.map(_.map(_.name).mkString(", ")).mkString("(", "", ")")
+  def toParams: Params = Params(paramss.flatten.map(ep => Param(ep.name, ep.typeUnqualified)))
