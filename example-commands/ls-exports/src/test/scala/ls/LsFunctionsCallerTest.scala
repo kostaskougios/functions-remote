92679f1f480056099cab4e259f28266d7d1f10b3
Kostas Kougios <kostas.kougios@googlemail.com>
2023-07-25 17:20:55 +0100

    -

--- example-commands/ls-exports/src/test/scala/ls/LsFunctionsCallerTest.scala
+++ example-commands/ls-exports/src/test/scala/ls/LsFunctionsCallerTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers.*
 
 class LsFunctionsCallerTest extends AnyFunSuiteLike {
-  test("calls toByteArray"):
+  test("chains calls correctly"):
     val byteData                                = Array(1, 2).map(_.toByte)
     val expected                                = LsResult(Seq(LsFile("test")))
     def toByteArray(p: LsFunctionsMethodParams) =
