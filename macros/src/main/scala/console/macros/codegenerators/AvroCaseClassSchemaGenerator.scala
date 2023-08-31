3328f27ec8d42cc06fd55e32b1426e3e0d8f9b27
Kostas Kougios <kostas.kougios@googlemail.com>
2023-08-31 17:28:30 +0100

    -

--- macros/src/main/scala/console/macros/codegenerators/AvroCaseClassSchemaGenerator.scala
+++ macros/src/main/scala/console/macros/codegenerators/AvroCaseClassSchemaGenerator.scala
@@ -1,12 +1,13 @@
 package console.macros.codegenerators
 
-import console.macros.codegenerators.MethodToCaseClassGenerator
 import console.macros.codegenerators.model.MethodCaseClass
 import console.macros.model.*
-import org.simplified.templates.ScalaFileTemplate
-import org.simplified.templates.model.{FileTemplatesSourceLocation, Imports}
+import mustache.integration.{Many, MustacheTemplate}
+import org.simplified.templates.model.ResourceTemplatesSourceLocation
 
-class AvroCaseClassSchemaGenerator(namingConventions: MethodToCaseClassGenerator.NamingConventions, scalaFileTemplate: ScalaFileTemplate) extends CodeGenerator:
+import scala.language.implicitConversions
+
+class AvroCaseClassSchemaGenerator(namingConventions: MethodToCaseClassGenerator.NamingConventions, template: MustacheTemplate) extends CodeGenerator:
   override def apply(packages: Seq[EPackage]): Seq[Code] = packages.flatMap(apply)
   def apply(`package`: EPackage): Seq[Code]              = `package`.types.map(apply(`package`, _))
 
@@ -16,20 +17,20 @@ class AvroCaseClassSchemaGenerator(namingConventions: MethodToCaseClassGenerator
 
     case class Vals(
         proxypackage: String,
-        imports: Imports,
+        imports: Many[String],
         functionsMethodAvroSerializer: String,
         methodParams: String,
-        caseClasses: Seq[MethodCaseClass]
+        caseClasses: Many[MethodCaseClass]
     )
     val n = namingConventions.caseClassHolderObjectName(`type`) + "AvroSerializer"
     Code(
       s"${`package`.toPath}/$n.scala",
-      scalaFileTemplate(Vals(`package`.name, Imports(Set(mpt + ".*")), n, mpt, caseClasses))
+      template(Vals(`package`.name, Set(mpt + ".*"), n, mpt, caseClasses))
     )
 
 object AvroCaseClassSchemaGenerator:
   def apply(namingConventions: MethodToCaseClassGenerator.NamingConventions = MethodToCaseClassGenerator.DefaultNamingConventions) =
     new AvroCaseClassSchemaGenerator(
       namingConventions,
-      ScalaFileTemplate(FileTemplatesSourceLocation("../proxy-templates/src/main/scala"), "proxypackage.FunctionsMethodAvroSerializer")
+      MustacheTemplate(ResourceTemplatesSourceLocation, "proxypackage.FunctionsMethodAvroSerializer")
     )
---
+++ proxy-templates/src/main/resources/proxypackage/FunctionsMethodAvroSerializer.scala
@@ -0,0 +1,24 @@
+package {{proxypackage}}
+
+import com.sksamuel.avro4s.AvroOutputStream
+import com.sksamuel.avro4s.AvroOutputStreamBuilder
+import java.io.ByteArrayOutputStream
+import scala.util.Using
+
+{{#imports}}
+import {{.}}
+{{/imports}}
+
+object {{functionsMethodAvroSerializer}}:
+  private def avroSerialize[A](b: AvroOutputStreamBuilder[A], value: A): Array[Byte] =
+    val bos = new ByteArrayOutputStream(4096)
+    Using.resource(b.to(bos).build()): os =>
+      os.write(value)
+
+    bos.toByteArray
+
+  val avroSerializer: PartialFunction[{{methodParams}}, Array[Byte]] =
+    {{#caseClasses}}
+    case value: {{caseClass}} =>
+      avroSerialize(AvroOutputStream.data[{{caseClass}}], value)
+    {{/caseClasses}}
