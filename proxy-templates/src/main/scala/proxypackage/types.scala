9cfa2749a0ee56e6437e3269d762644abe595e2a
Kostas Kougios <kostas.kougios@googlemail.com>
2023-08-31 17:29:13 +0100

    -

--- proxy-templates/src/main/scala/proxypackage/types.scala
+++
@@ -1,9 +0,0 @@
-package proxypackage
-
-// This is not a template, it just mocks some types to make the templates compile
-
-trait resultN
-
-case class caseClass(s: String) extends `methodParams`
-
-trait function1ReturnType
