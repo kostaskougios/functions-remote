2aebd13637028f61cbfbb3a464a1a7b5dc4789f6
Kostas Kougios <kostas.kougios@googlemail.com>
2023-09-12 23:29:57 +0100

    -

--- macros/src/main/scala/console/macros/codegenerators/CallerGenerator.scala
+++ macros/src/main/scala/console/macros/codegenerators/CallerGenerator.scala
@@ -7,10 +7,6 @@ import mustache.integration.model.ResourceTemplatesSourceLocation
 
 import scala.language.implicitConversions
 
-/** Converts a trait A to a class that proxies A's methods. Each proxy converts the method's args to a case class and passes it through 2 functions.
-  *
-  * Example: function 1 converts the case class to json and function 2 does a rest api call
-  */
 object CallerGenerator:
   object DefaultNamingConventions extends GenericTypeGenerator.NamingConventions:
     def className(`type`: EType) = s"${`type`.name}Caller"
