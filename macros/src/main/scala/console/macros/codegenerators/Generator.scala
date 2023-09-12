a85735c6315c169ab2bfb59c6f8bf72668269513
Kostas Kougios <kostas.kougios@googlemail.com>
2023-09-12 17:28:28 +0100

    -

---
+++ macros/src/main/scala/console/macros/codegenerators/Generator.scala
@@ -0,0 +1,53 @@
+package console.macros.codegenerators
+
+import console.macros.codegenerators.model.Func
+import console.macros.model.{Code, EPackage, EType}
+import mustache.integration.MustacheTemplate
+import mustache.integration.model.Many
+import scala.language.implicitConversions
+
+class Generator(
+    config: Generator.Config,
+    template: MustacheTemplate
+) extends CodeGenerator:
+  override def apply(packages: Seq[EPackage]): Seq[Code] =
+    packages.flatMap(apply)
+
+  def apply(`package`: EPackage): Seq[Code] =
+    `package`.types.map(apply(`package`, _))
+
+  def apply(`package`: EPackage, `type`: EType): Code =
+    case class Vals(
+        proxypackage: String,
+        imports: Many[String],
+        functionsReceiver: String,
+        methodParams: String,
+        functions: Many[Func]
+    )
+    val imports   = `type`.typesInMethods.toSet
+    val sn        = config.namingConventions.className(`type`)
+    val mpt       = config.methodToCaseClassNamingConventions.methodParamsTraitName(`type`)
+    val functions = Func(`type`, config.methodToCaseClassNamingConventions)
+
+    val vals = Vals(`package`.name, imports, sn, mpt, functions)
+    val code = template(vals)
+    Code(
+      s"${`package`.toPath}/$sn.scala",
+      code
+    )
+
+object Generator:
+  trait NamingConventions:
+    /** The name of the generated caller class
+      *
+      * @param `type`
+      *   the type where the method belongs to
+      * @return
+      *   the case class name for the method
+      */
+    def className(`type`: EType): String
+
+  case class Config(
+      methodToCaseClassNamingConventions: MethodToCaseClassGenerator.NamingConventions = MethodToCaseClassGenerator.DefaultNamingConventions,
+      namingConventions: NamingConventions
+  )
--- macros/src/main/scala/console/macros/codegenerators/ReceiverGenerator.scala
+++ macros/src/main/scala/console/macros/codegenerators/ReceiverGenerator.scala
@@ -1,66 +1,17 @@
 package console.macros.codegenerators
 
-import console.macros.codegenerators.ReceiverGenerator.Config
-import console.macros.codegenerators.model.Func
+import console.macros.codegenerators.Generator.Config
 import console.macros.model.*
 import mustache.integration.MustacheTemplate
-import mustache.integration.model.{Many, Param, ResourceTemplatesSourceLocation}
-
-import scala.language.implicitConversions
-
-/** Converts a trait A to a class that proxies A's methods. Each proxy converts the method's args to a case class and passes it through 2 functions.
-  *
-  * Example: function 1 converts the case class to json and function 2 does a rest api call
-  */
-class ReceiverGenerator(
-    config: Config,
-    template: MustacheTemplate
-) extends CodeGenerator:
-  override def apply(packages: Seq[EPackage]): Seq[Code] =
-    packages.flatMap(apply)
-
-  def apply(`package`: EPackage): Seq[Code] =
-    `package`.types.map(apply(`package`, _))
-
-  def apply(`package`: EPackage, `type`: EType): Code =
-    case class Vals(
-        proxypackage: String,
-        imports: Many[String],
-        functionsReceiver: String,
-        methodParams: String,
-        functions: Many[Func]
-    )
-    val imports   = `type`.typesInMethods.toSet
-    val sn        = config.traitToSenderNamingConventions.className(`type`)
-    val mpt       = config.methodToCaseClassNamingConventions.methodParamsTraitName(`type`)
-    val functions = Func(`type`, config.methodToCaseClassNamingConventions)
-
-    val vals = Vals(`package`.name, imports, sn, mpt, functions)
-    val code = template(vals)
-    Code(
-      s"${`package`.toPath}/$sn.scala",
-      code
-    )
+import mustache.integration.model.ResourceTemplatesSourceLocation
 
 object ReceiverGenerator:
-  case class Config(
-      methodToCaseClassNamingConventions: MethodToCaseClassGenerator.NamingConventions = MethodToCaseClassGenerator.DefaultNamingConventions,
-      traitToSenderNamingConventions: ReceiverGenerator.NamingConventions = DefaultNamingConventions
-  )
-  trait NamingConventions:
-    /** The name of the generated caller class
-      * @param `type`
-      *   the type where the method belongs to
-      * @return
-      *   the case class name for the method
-      */
+  object DefaultNamingConventions extends Generator.NamingConventions:
     def className(`type`: EType) = s"${`type`.name}Receiver"
 
-  object DefaultNamingConventions extends NamingConventions
-
   def apply(
-      config: Config = Config()
-  ) = new ReceiverGenerator(
+      config: Config = Config(namingConventions = DefaultNamingConventions)
+  ) = new Generator(
     config,
     MustacheTemplate(ResourceTemplatesSourceLocation, "proxypackage.FunctionsReceiver")
   )
