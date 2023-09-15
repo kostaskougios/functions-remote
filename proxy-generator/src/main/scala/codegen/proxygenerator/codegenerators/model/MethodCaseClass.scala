a2be60a8039116daa392405a506782484924dbb2
Kostas Kougios <kostas.kougios@googlemail.com>
2023-09-16 00:01:31 +0100

    -

--- proxy-generator/src/main/scala/codegen/proxygenerator/codegenerators/model/MethodCaseClass.scala
+++ proxy-generator/src/main/scala/codegen/proxygenerator/codegenerators/model/MethodCaseClass.scala
@@ -1,7 +1,4 @@
 package codegen.proxygenerator.codegenerators.model
 
-import codegen.proxygenerator.codegenerators.GenericTypeGenerator.NamingConventions
-import codegen.tastyextractor.model.{EMethod, EPackage, EType}
-
 // method parameters converted to this so that we can render it
 case class MethodCaseClass(imports: Set[String], caseClass: String, paramsDecl: String)
