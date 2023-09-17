c65a1d4442857ef3bba2a75e6ac2ffb61f4b295b
Kostas Kougios <kostas.kougios@googlemail.com>
2023-09-17 17:05:06 +0100

    -

--- example-commands/ls-exports/src/test/scala/ls/LsFunctionsTest.scala
+++ example-commands/ls-exports/src/test/scala/ls/LsFunctionsTest.scala
@@ -1,20 +1,38 @@
 package ls
 
-import ls.model.{LsFile, LsResult}
+import ls.LsFunctionsMethods.Ls
+import ls.model.{LsFile, LsOptions, LsResult}
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers.*
 
 class LsFunctionsTest extends AnyFunSuiteLike:
   val serializer = new LsFunctionsAvroSerializer
+  val someResult = LsResult(Seq(LsFile("f1"), LsFile("f2")))
+
   test("caller end to end") {
     new CallerApp:
-      val result = LsResult(Seq(LsFile("f1"), LsFile("f2")))
 
       override def transport(method: String, input: Array[Byte]) =
-        serializer.lsReturnTypeSerializer(result)
+        serializer.lsReturnTypeSerializer(someResult)
 
-      caller.ls("/tmp") should be(result)
+      caller.ls("/tmp") should be(someResult)
   }
+
+  test("receiver end to end") {
+    val lsFunctions = new UnimplementedLsFunctions:
+      override def ls(path: String, lsOptions: LsOptions) =
+        path should be("/tmp")
+        lsOptions should be(LsOptions.Defaults)
+        someResult
+
+    new ReceiverApp(lsFunctions):
+      val received = receiver.ls(serializer.lsSerializer(Ls("/tmp", LsOptions.Defaults)))
+      serializer.lsReturnTypeDeserializer(received) should be(someResult)
+  }
+
   abstract class CallerApp:
     def transport(method: String, input: Array[Byte]): Array[Byte]
     val caller = LsFunctionsCallerAvroSerializedFactory.createLsFunctionsCaller(transport)
+
+  abstract class ReceiverApp(functions: LsFunctions):
+    val receiver = LsFunctionsReceiverAvroSerializedFactory.createLsFunctionsReceiver(functions)
