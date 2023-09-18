4928ab9487b207d3886c3dfaa3f8ed82cbcaffb1
Kostas Kougios <kostas.kougios@googlemail.com>
2023-09-18 20:33:57 +0100

    -

--- proxy-templates/src/main/resources/proxypackage/FunctionsCallerAvroSerializedFactory.scala
+++ proxy-templates/src/main/resources/proxypackage/FunctionsCallerAvroSerializedFactory.scala
@@ -1,6 +1,11 @@
 package {{proxypackage}}
 
 object {{className}}:
+  def createCaller(transport: (String, Array[Byte]) => Array[Byte]): PartialFunction[String, {{exportedType.name}}] =
+      {{#functions}}
+      case {{exportedType.name}}Methods.Methods.{{caseClassName}} => create{{exportedType.name}}Caller(transport)
+      {{/functions}}
+
   def create{{exportedType.name}}Caller(transport: (String, Array[Byte]) => Array[Byte]): {{exportedType.name}} =
     val serializer = new {{exportedType.name}}AvroSerializer
     new {{exportedType.name}}Caller(
