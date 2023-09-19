954186f5e5309cb2b519df54c149d73e61c0dd5a
Kostas Kougios <kostas.kougios@googlemail.com>
2023-09-19 18:07:49 +0100

    -

--- example-commands/using-commands/src/main/scala/example/TryLs.scala
+++ example-commands/using-commands/src/main/scala/example/TryLs.scala
@@ -1,12 +1,15 @@
 package example
 
 import functions.discovery.FunctionsDiscovery
-import ls.{LsFunctions, LsFunctionsCallerAvroSerializedFactory}
+import ls.{LsFunctions, LsFunctionsAvroSerializer}
+import ls.model.{LsFile, LsResult}
 
 @main def tryLs() =
   val discovery = FunctionsDiscovery { (cmd, data) =>
     println(s"running $cmd")
-    Array.emptyByteArray
+    val serializer = new LsFunctionsAvroSerializer
+    serializer.lsReturnTypeSerializer(LsResult(Seq(LsFile("/tmp/1"))))
   }
   val functions = discovery.discoverFirstOne[LsFunctions]
-  functions.ls("/tmp")
+  val result    = functions.ls("/tmp")
+  println(result)
