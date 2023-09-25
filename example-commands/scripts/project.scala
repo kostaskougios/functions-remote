aaa8079b9712d8659be8ee3a71825e048f5121f2
Kostas Kougios <kostas.kougios@googlemail.com>
2023-09-26 00:46:10 +0100

    -

--- example-commands/scripts/project.scala
+++ example-commands/scripts/project.scala
@@ -1,2 +1,2 @@
 //> using dependency com.lihaoyi::os-lib:0.9.1
-//> using dependency org.kkougios::proxy-generator:0.1-SNAPSHOT
+//> using dependency org.functions-remote::proxy-generator:0.1-SNAPSHOT
