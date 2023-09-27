e828ee1764a03adbddde5554be173944481ab753
Kostas Kougios <kostas.kougios@googlemail.com>
2023-09-28 00:37:25 +0100

    -

--- coursier/src/main/scala/functions/coursier/CoursierResolver.scala
+++ coursier/src/main/scala/functions/coursier/CoursierResolver.scala
@@ -1,6 +1,13 @@
 package functions.coursier
 import coursier._
 import coursier.params.ResolutionParams
+import functions.coursier.utils.Env.FunctionsHome
+
+import java.io.File
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import scala.io.Source
+import scala.util.Using
 
 // see https://get-coursier.io/docs/api
 class CoursierResolver {
@@ -19,7 +26,16 @@ class CoursierResolver {
 
 object CoursierResolver extends App {
   val resolver = new CoursierResolver
-  val d        = dep"org.functions-remote:proxy-generator_3:0.1-SNAPSHOT"
-  val r        = resolver.resolve(d)
-  println(r.mkString("\n"))
+  val deps     = Using.resource(Source.fromFile(new File(FunctionsHome + "/import-functions.dep"), "UTF-8"))(_.getLines().toList)
+
+  for {
+    dep <- deps.filterNot(_.isBlank)
+  } {
+    println(s"Importing $dep")
+    val Array(groupId, artifactId, version) = dep.split(":")
+    val d                                   = Dependency(Module(Organization(groupId), ModuleName(artifactId)), version)
+    val r                                   = resolver.resolve(d)
+    val output                              = r.mkString("\n")
+    Files.write(new File(FunctionsHome + s"/.dependencies/$dep.classpath").toPath, output.getBytes(StandardCharsets.UTF_8))
+  }
 }
---
+++ coursier/src/main/scala/functions/coursier/utils/Env.scala
@@ -0,0 +1,9 @@
+package functions.coursier.utils
+
+object Env {
+  private val ConfigDir = "functions-remote.config.dir"
+  val FunctionsHome     = sys.env.getOrElse(
+    ConfigDir,
+    throw new IllegalStateException(s"Please set env variable $ConfigDir to point to the config directory of functions-remote")
+  )
+}
