574e18c15bf81eeca779a6ed14f708832841a642
Kostas Kougios <kostas.kougios@googlemail.com>
2023-09-27 15:42:45 +0100

    -

--- coursier/src/main/scala/functions/coursier/CoursierResolver.scala
+++ coursier/src/main/scala/functions/coursier/CoursierResolver.scala
@@ -1,16 +1,25 @@
 package functions.coursier
 import coursier._
+import coursier.params.ResolutionParams
 
+// see https://get-coursier.io/docs/api
 class CoursierResolver {
-  def resolve(group: String, artifact: String, version: String) = {
-    Resolve()
-      .addDependencies(Dependency(Module(Organization(group), ModuleName(artifact)), version))
+  def resolve(dependency: Dependency) = {
+//    val params = ResolutionParams()
+//      .withScalaVersion("3.3.1")
+//    Resolve()
+//      .addDependencies(dependency)
+//      .withResolutionParams(params)
+//      .run()
+    Fetch()
+      .addDependencies(dependency)
       .run()
   }
 }
 
 object CoursierResolver extends App {
   val resolver = new CoursierResolver
-  val r        = resolver.resolve("org.functions-remote", "proxy-generator_3", "0.1-SNAPSHOT")
-  println(r.dependencies.mkString("\n"))
+  val d        = dep"org.functions-remote:proxy-generator_3:0.1-SNAPSHOT"
+  val r        = resolver.resolve(d)
+  println(r.mkString("\n"))
 }
