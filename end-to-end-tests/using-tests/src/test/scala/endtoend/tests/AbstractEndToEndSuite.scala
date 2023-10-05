66c0a9b7790cf4e9646feefd1aee9a874a74a681
Kostas Kougios <kostas.kougios@googlemail.com>
2023-10-05 18:56:45 +0100

    -

--- end-to-end-tests/using-tests/src/test/scala/endtoend/tests/AbstractEndToEndSuite.scala
+++ end-to-end-tests/using-tests/src/test/scala/endtoend/tests/AbstractEndToEndSuite.scala
@@ -7,6 +7,10 @@ import org.scalatest.funsuite.AnyFunSuite
 import java.io.File
 
 class AbstractEndToEndSuite extends AnyFunSuite:
-  def configPath = new File("../config")
+  def configPath: File =
+    val insideIntelliJ = new File("../config")
+    if insideIntelliJ.exists then insideIntelliJ
+    else new File("end-to-end-tests/config") // sbt
+
   println(s"config path: $configPath")
-  val discovery  = FunctionsDiscovery(RuntimeConfig.withDefaults(configRootDirectory = configPath.getAbsolutePath))
+  val discovery = FunctionsDiscovery(RuntimeConfig.withDefaults(configRootDirectory = configPath.getAbsolutePath))
--- end-to-end-tests/using-tests/src/test/scala/endtoend/tests/SimpleFunctionsSuite.scala
+++ end-to-end-tests/using-tests/src/test/scala/endtoend/tests/SimpleFunctionsSuite.scala
@@ -1,7 +1,5 @@
 package endtoend.tests
 
-import functions.discovery.FunctionsDiscovery
-import functions.model.RuntimeConfig
 import org.scalatest.matchers.should.Matchers.*
 
 class SimpleFunctionsSuite extends AbstractEndToEndSuite:
