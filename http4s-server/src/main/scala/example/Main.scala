84a36c7d9e11590cd6481ada6b0f10832e86235d
Kostas Kougios <kostas.kougios@googlemail.com>
2023-10-06 14:43:17 +0100

    -

--- http4s-server/src/main/scala/example/Main.scala
+++ http4s-server/src/main/scala/example/Main.scala
@@ -58,19 +58,15 @@ trait HelloWorld[F[_]]:
 object HelloWorld:
   final case class Name(name: String) extends AnyVal
 
-  /** More generally you will want to decouple your edge representations from your internal data structures, however this shows how you can create encoders for
-    * your data.
-    */
   final case class Greeting(greeting: String) extends AnyVal
   object Greeting:
-    given Encoder[Greeting] = new Encoder[Greeting]:
-      final def apply(a: Greeting): Json = Json.obj(
-        ("message", Json.fromString(a.greeting))
-      )
+    given Encoder[Greeting] =
+      a =>
+        Json.obj(
+          ("message", Json.fromString(a.greeting))
+        )
 
-    given [F[_]]: EntityEncoder[F, Greeting] =
-      jsonEncoderOf[F, Greeting]
+    given [F[_]]: EntityEncoder[F, Greeting] = jsonEncoderOf[F, Greeting]
 
-  def impl[F[_]: Applicative]: HelloWorld[F] = new HelloWorld[F]:
-    def hello(n: HelloWorld.Name): F[HelloWorld.Greeting] =
-      Greeting("Hello, " + n.name).pure[F]
+  def impl[F[_]: Applicative]: HelloWorld[F] =
+    n => Greeting("Hello, " + n.name).pure[F]
