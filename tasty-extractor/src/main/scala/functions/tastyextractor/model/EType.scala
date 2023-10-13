629a071ede3b297b5e536fcc4baf364166921970
Kostas Kougios <kostas.kougios@googlemail.com>
2023-10-13 13:39:34 +0100

    -

--- tasty-extractor/src/main/scala/functions/tastyextractor/model/EType.scala
+++ tasty-extractor/src/main/scala/functions/tastyextractor/model/EType.scala
@@ -4,7 +4,14 @@ import org.apache.commons.lang3.StringUtils
 
 case class EType(name: String, code: String, scalaDocs: Option[String], methods: Seq[EMethod]):
   def asImport: String                      = if code.contains('[') then StringUtils.substringBefore(code, "[") else code
-  def importsForTypesInMethods: Seq[String] = methods.flatMap(m => m.paramss.flatMap(_.map(_.`type`.asImport) :+ m.returnType.asImport)).distinct
+  def importsForTypesInMethods: Seq[String] =
+    methods
+      .flatMap(m => m.paramss.flatMap(_.map(_.`type`) :+ m.returnType))
+      .filterNot(_.isAlwaysImported)
+      .map(_.asImport)
+      .distinct
+
+  def isAlwaysImported: Boolean = asImport.startsWith("scala.") && !asImport.substring(7).contains('.')
 
 object EType:
   def code(name: String, code: String) = EType(name, code, None, Nil)
