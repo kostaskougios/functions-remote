e91360078a851b5a6ed6f659b5d1ba3394eae31d
Kostas Kougios <kostas.kougios@googlemail.com>
2023-10-17 23:27:10 +0100

    -

--- tasty-extractor/src/main/scala/functions/tastyextractor/model/EType.scala
+++ tasty-extractor/src/main/scala/functions/tastyextractor/model/EType.scala
@@ -1,7 +1,6 @@
 package functions.tastyextractor.model
 
 import org.apache.commons.lang3
-import org.apache.commons.lang3.StringUtils
 
 case class EType(name: String, code: String, typeArgs: Seq[EType], framework: Option[DetectedFramework], scalaDocs: Option[String], methods: Seq[EMethod]):
   def frameworkName: String  = framework.map(_.frameworkName).getOrElse("none")
