921ceddf2332de07c94182273ab716d53b484ae9
Kostas Kougios <kostas.kougios@googlemail.com>
2023-10-18 16:52:56 +0100

    -

--- end-to-end-tests/tests-http4s-server-impl/src/main/scala/example/Http4sServerExampleMain.scala
+++ end-to-end-tests/tests-http4s-server-impl/src/main/scala/example/Http4sServerExampleMain.scala
@@ -2,15 +2,18 @@ package example
 
 import cats.effect.{Async, IO, IOApp, Sync}
 import com.comcast.ip4s.*
+import endtoend.tests.cats.{TestsCatsFunctionsImpl, TestsCatsFunctionsReceiverCirceJsonSerializedFactory}
 import fs2.io.net.Network
 import functions.http4s.RequestProcessor
 import functions.receiver.FunctionsInvoker
 import functions.receiver.model.RegisteredFunction
 import org.http4s.dsl.Http4sDsl
 import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.headers.`Content-Type`
 import org.http4s.implicits.*
 import org.http4s.server.middleware.Logger
-import org.http4s.{HttpRoutes, Request, Response}
+import org.http4s.{HttpRoutes, MediaType, Request, Response}
+import cats.syntax.all.*
 
 object Http4sServerExampleMain extends IOApp.Simple:
   val run = QuickstartServer.run[IO]
@@ -33,11 +36,17 @@ object QuickstartServer:
     } yield ()
   }.useForever
 
-def simpleFunctionsRoutes[F[_]: Sync]: HttpRoutes[F] =
+def simpleFunctionsRoutes[F[_]: Async]: HttpRoutes[F] =
 //  val invoker          = FunctionsInvoker.withFunctions(RegisteredFunction[SimpleFunctions](new SimpleFunctionsImpl))
 //  val requestProcessor = new RequestProcessor[F](invoker)
-  val dsl = new Http4sDsl[F] {}
+  val receiver = TestsCatsFunctionsReceiverCirceJsonSerializedFactory.createReceiver[F](new TestsCatsFunctionsImpl[F])
+  val dsl      = new Http4sDsl[F] {}
   import dsl.*
 
   HttpRoutes.of[F]:
-    case req @ PUT -> Root / format / method => Ok("Hello")
+    case req @ PUT -> Root / "catsAddR" =>
+      val r = for
+        inData <- req.body.compile.to(Array)
+        res    <- receiver.catsAddR(inData)
+      yield res
+      Ok(r).map(_.withContentType(`Content-Type`(MediaType.application.`octet-stream`)))
