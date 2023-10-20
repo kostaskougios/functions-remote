41831ffb659dda1d103b790a6824755ea2425633
Kostas Kougios <kostas.kougios@googlemail.com>
2023-10-20 16:45:39 +0100

    -

--- proxy-generator/src/main/resources/proxypackage/http4s/Routes.scala
+++ proxy-generator/src/main/resources/proxypackage/http4s/Routes.scala
@@ -22,8 +22,9 @@ class {{className}}{{frameworkTypeArgFull}}(
   val allRoutes: PartialFunction[Request{{exportedTypeTypeArgs}}, {{frameworkTypeArg}}[Response{{exportedTypeTypeArgs}}]] = {{#functions}}{{functionN}} {{^last}}orElse{{/last}} {{/functions}}
 
   {{#functions}}
+  private val {{functionN}}Path = pathFor("{{proxypackage}}.{{exportedType.name}}","{{functionN}}")
   def {{functionN}}: PartialFunction[Request{{exportedTypeTypeArgs}}, {{frameworkTypeArg}}[Response{{exportedTypeTypeArgs}}]] =
-    case req @ PUT -> p if p == pathFor("{{proxypackage}}.{{exportedType.name}}","{{functionN}}") =>
+    case req @ PUT -> p if p == {{functionN}}Path =>
       val r = for
         inData <- req.body.compile.to(Array)
         res    <- receiver.{{functionN}}(inData)
