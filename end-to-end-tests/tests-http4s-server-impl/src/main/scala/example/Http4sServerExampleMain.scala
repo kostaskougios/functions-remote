78d96de3a958bb6d6dd07340cee236207903fd96
Kostas Kougios <kostas.kougios@googlemail.com>
2023-10-20 13:18:06 +0100

    -

--- end-to-end-tests/tests-http4s-server-impl/src/main/scala/example/Http4sServerExampleMain.scala
+++ end-to-end-tests/tests-http4s-server-impl/src/main/scala/example/Http4sServerExampleMain.scala
@@ -2,7 +2,12 @@ package example
 
 import cats.effect.{Async, IO, IOApp}
 import com.comcast.ip4s.*
-import endtoend.tests.cats.{TestsCatsFunctionsHttp4sRoutes, TestsCatsFunctionsImpl, TestsCatsFunctionsReceiverCirceJsonSerializedFactory}
+import endtoend.tests.cats.{
+  TestsCatsFunctionsHttp4sRoutes,
+  TestsCatsFunctionsImpl,
+  TestsCatsFunctionsReceiverAvroSerializedFactory,
+  TestsCatsFunctionsReceiverCirceJsonSerializedFactory
+}
 import fs2.io.net.Network
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.headers.`Content-Type`
@@ -16,10 +21,13 @@ object Http4sServerExampleMain extends IOApp.Simple:
 object QuickstartServer:
 
   def run[F[_]: Async: Network]: F[Nothing] = {
-    val receiver   = TestsCatsFunctionsReceiverCirceJsonSerializedFactory.createReceiver[F](new TestsCatsFunctionsImpl[F])
-    val testRoutes = new TestsCatsFunctionsHttp4sRoutes[F](receiver, `Content-Type`(MediaType.application.`octet-stream`))
-    val routes     = HttpRoutes.of[F](testRoutes.allRoutes)
-    val httpApp    = routes.orNotFound
+    val impl           = new TestsCatsFunctionsImpl[F]
+    val jsonReceiver   = TestsCatsFunctionsReceiverCirceJsonSerializedFactory.createReceiver[F](impl)
+    val avroReceiver   = TestsCatsFunctionsReceiverAvroSerializedFactory.createReceiver[F](impl)
+    val testRoutesJson = new TestsCatsFunctionsHttp4sRoutes[F](jsonReceiver, `Content-Type`(MediaType.application.json))
+    val testRoutesAvro = new TestsCatsFunctionsHttp4sRoutes[F](avroReceiver, `Content-Type`(MediaType.application.`octet-stream`), "Avro")
+    val routes         = HttpRoutes.of[F](testRoutesJson.allRoutes orElse testRoutesAvro.allRoutes)
+    val httpApp        = routes.orNotFound
 
     // With Middlewares in place
     val finalHttpApp = Logger.httpApp(true, true)(httpApp)
