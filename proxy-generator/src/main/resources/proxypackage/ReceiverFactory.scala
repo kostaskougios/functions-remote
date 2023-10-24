021c8b76f8da67ed08802223a4226317e72af92e
Kostas Kougios <kostas.kougios@googlemail.com>
2023-10-25 00:39:27 +0100

    -

--- proxy-generator/src/main/resources/proxypackage/ReceiverFactory.scala
+++ proxy-generator/src/main/resources/proxypackage/ReceiverFactory.scala
@@ -19,7 +19,7 @@ object {{className}}:
   // Http4s factories
   {{#generatorFactories.serializers}}
   // {{.}} serialized routes
-  def new{{.}}{{exportedType.name}}Routes{{frameworkTypeArgFull}}(impl: {{exportedTypeFull}}) =
+  def new{{.}}{{exportedType.name}}Routes{{frameworkTypeArgFull}}(impl: {{exportedTypeFull}}): {{exportedType.name}}Http4sRoutes{{exportedTypeTypeArgs}} =
     val receiver = new{{.}}{{exportedType.name}}{{exportedTypeTypeArgs}}(impl)
     new {{exportedType.name}}Http4sRoutes{{exportedTypeTypeArgs}}(receiver, Serializer.{{.}})
 
