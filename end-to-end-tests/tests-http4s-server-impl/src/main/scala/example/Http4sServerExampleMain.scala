e15c558ce043d04005aad2a7b9ee14c0e2708dd1
Kostas Kougios <kostas.kougios@googlemail.com>
2023-10-27 00:24:34 +0100

    -

--- end-to-end-tests/tests-http4s-server-impl/src/main/scala/example/Http4sServerExampleMain.scala
+++ end-to-end-tests/tests-http4s-server-impl/src/main/scala/example/Http4sServerExampleMain.scala
@@ -2,34 +2,13 @@ package example
 
 import cats.effect.{Async, IO, IOApp}
 import com.comcast.ip4s.*
-import endtoend.tests.cats.TestsCatsFunctionsReceiverFactory
 import fs2.io.net.Network
-import org.http4s.HttpRoutes
-import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.implicits.*
-import org.http4s.server.middleware.Logger
-import tests.cats.TestsCatsFunctionsImpl
+import tests.cats.Server
 
 object Http4sServerExampleMain extends IOApp.Simple:
   val run = QuickstartServer.run[IO]
 
 object QuickstartServer:
 
-  def run[F[_]: Async: Network]: F[Nothing] = {
-    val impl           = new TestsCatsFunctionsImpl[F]
-    val testRoutesJson = TestsCatsFunctionsReceiverFactory.newJsonTestsCatsFunctionsRoutes(impl)
-    val testRoutesAvro = TestsCatsFunctionsReceiverFactory.newAvroTestsCatsFunctionsRoutes(impl)
-    val routes         = HttpRoutes.of[F](testRoutesJson.allRoutes orElse testRoutesAvro.allRoutes)
-    val httpApp        = routes.orNotFound
-
-    val finalHttpApp = Logger.httpApp(true, true)(httpApp)
-    for {
-      _ <-
-        EmberServerBuilder
-          .default[F]
-          .withHost(ipv4"0.0.0.0")
-          .withPort(port"8080")
-          .withHttpApp(finalHttpApp)
-          .build
-    } yield ()
-  }.useForever
+  def run[F[_]: Async: Network]: F[Nothing] =
+    Server.newServer(port"8080").useForever
