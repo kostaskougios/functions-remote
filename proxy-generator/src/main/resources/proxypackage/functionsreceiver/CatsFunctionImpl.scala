02a6fe5f7b6f16387f67d99c76a99fbcc5694cdd
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-08 18:10:56 +0000

    -

--- proxy-generator/src/main/resources/proxypackage/functionsreceiver/CatsFunctionImpl.scala
+++ proxy-generator/src/main/resources/proxypackage/functionsreceiver/CatsFunctionImpl.scala
@@ -1,6 +1,7 @@
   {{#mapResults}}
   def {{functionN}}{{firstParamsAndParens}}(data: Array[Byte]): {{frameworkTypeArg}}[Array[Byte]] =
-    val params = {{functionN}}Deserializer(data)
+    {{#paramsRaw.isEmpty}}if data.length>0 then throw new IllegalStateException(s"Expected empty array of bytes but got ${data.length}"){{/paramsRaw.isEmpty}}
+    {{^paramsRaw.isEmpty}}val params = {{functionN}}Deserializer(data) {{/paramsRaw.isEmpty}}
     f.{{functionN}}{{firstParamsCallAndParens}}({{#paramsRaw}}params.{{name}}{{^last}}, {{/last}}{{/paramsRaw}}).map: r=>
       {{^isUnitReturnType}}
       {{functionN}}ReturnTypeSerializer(r)
