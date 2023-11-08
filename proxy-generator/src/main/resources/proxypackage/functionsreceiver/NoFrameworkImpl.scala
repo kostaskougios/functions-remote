a23c75b3ab53129aa1b37c4c8d9a7d54d8b421c2
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-08 18:34:53 +0000

    -

--- proxy-generator/src/main/resources/proxypackage/functionsreceiver/NoFrameworkImpl.scala
+++ proxy-generator/src/main/resources/proxypackage/functionsreceiver/NoFrameworkImpl.scala
@@ -1,6 +1,7 @@
   {{^mapResults}}
   def {{functionN}}(data: Array[Byte]): Array[Byte] =
-    val params = {{functionN}}Deserializer(data)
+    {{#paramsRaw.isEmpty}}if data.length>0 then throw new IllegalStateException(s"Expected empty array of bytes but got ${data.length}"){{/paramsRaw.isEmpty}}
+    {{^paramsRaw.isEmpty}}val params = {{functionN}}Deserializer(data) {{/paramsRaw.isEmpty}}
     {{^isUnitReturnType}}
     val r      = f.{{functionN}}({{#paramsRaw}}params.{{name}}{{^last}}, {{/last}}{{/paramsRaw}})
     {{functionN}}ReturnTypeSerializer(r)
