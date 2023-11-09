fd79e92f500d499523a007b773e6a4bc5db6129a
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-09 13:12:54 +0000

    -

--- end-to-end-tests/kafka-producer/src/main/scala/example/KafkaProducer.scala
+++ end-to-end-tests/kafka-producer/src/main/scala/example/KafkaProducer.scala
@@ -1,11 +1,11 @@
 package example
 
-import endtoend.tests.kafka.KafkaFunctionsMethods
+import endtoend.tests.kafka.{KafkaFunctionsCallerFactory, KafkaFunctionsMethods}
 import endtoend.tests.kafka.model.Person
 import functions.model.Serializer
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.header.internals.RecordHeader
-import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 
 /** bin/kafka-topics.sh --create --topic people --bootstrap-server localhost:9092
   *
@@ -13,12 +13,17 @@ import org.apache.kafka.common.serialization.StringSerializer
   */
 @main
 def kafkaProducer() =
-  val producer = new KafkaProducer[String, KafkaFunctionsMethods.AddPerson](KafkaConf.props, new StringSerializer, new AddPersonSerializer)
-  try
-    val pr          = new ProducerRecord("add-person", "kostas", KafkaFunctionsMethods.AddPerson(1000, Person(1, "Kostas")))
+  val producer = new KafkaProducer(KafkaConf.props, new StringSerializer, new ByteArraySerializer)
+
+  val f = KafkaFunctionsCallerFactory.newAvroKafkaFunctions: trIn =>
     val coordinates = KafkaFunctionsMethods.Methods.AddPerson.withSerializer(Serializer.Avro)
+    val pr          = new ProducerRecord("add-person", "kostas", trIn.data)
     pr.headers()
       .add(new RecordHeader("coordinates", coordinates.toRawCoordinates.getBytes("UTF-8")))
     producer.send(pr)
+    Array.emptyByteArray
+
+  try
+    f.addPerson("kostas")(2000, Person(5, "KostasK"))
     println("OK")
   finally producer.close()
