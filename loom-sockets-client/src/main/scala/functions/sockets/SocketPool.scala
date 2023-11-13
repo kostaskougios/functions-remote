ebdcc334c12838dad4359a7b0e95c3880bcb3d89
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-13 20:21:50 +0000

    -

--- loom-sockets-client/src/main/scala/functions/sockets/SocketPool.scala
+++ loom-sockets-client/src/main/scala/functions/sockets/SocketPool.scala
@@ -1,48 +1,56 @@
 package functions.sockets
 
 import java.net.{InetAddress, Socket, SocketException}
+import java.util.concurrent.{ArrayBlockingQueue, BlockingQueue}
 import java.util.concurrent.atomic.AtomicInteger
+import scala.jdk.CollectionConverters.*
 
 class SocketPool(inetAddress: InetAddress, port: Int, poolSz: Int, retriesToOpenSocketBeforeGivingUp: Int):
-  private val available = scala.collection.mutable.Stack.empty[Socket]
-  private val used      = new AtomicInteger(0)
+  private class Sock(@volatile var socket: Option[Socket] = None):
+    def allocateSocket: Socket =
+      if socket.isEmpty then
+        val s = createSocket(retriesToOpenSocketBeforeGivingUp)
+        socket = Some(s)
+        s
+      else socket.get
 
-  def idleSockets: Seq[Socket] = available.synchronized(available.toList)
-  def activeSockets: Int       = used.get()
+    def invalidateSocket(): Unit =
+      for s <- socket do doAndPrintError(s.close())
+      socket = None
 
-  private def createSocket(retries: Int): Socket =
-    if retries > 0 then
-      try new Socket(inetAddress, port)
-      catch
-        case t: SocketException =>
-          Thread.`yield`()
-          createSocket(retries - 1)
-    else throw new IllegalStateException(s"Can't create socket to $inetAddress:$port")
+    private def createSocket(retries: Int): Socket =
+      if retries > 0 then
+        try new Socket(inetAddress, port)
+        catch
+          case t: SocketException =>
+            Thread.`yield`()
+            createSocket(retries - 1)
+      else throw new IllegalStateException(s"Can't create socket to $inetAddress:$port")
+
+  private val available = new ArrayBlockingQueue[Sock](poolSz)
+  for _ <- 1 to poolSz do available.add(Sock())
+
+  def idleSockets: Seq[Socket] = available.asScala.flatMap(_.socket).toList
+  def activeSockets: Int       = poolSz - idleSockets.size
 
   def withSocket[R](f: Socket => R): R =
-    val s = available.synchronized:
-      if available.isEmpty then createSocket(retriesToOpenSocketBeforeGivingUp)
-      else available.pop()
-
-    used.incrementAndGet()
-    try
-      val r = f(s)
-      available.synchronized(available.push(s))
-      used.decrementAndGet()
-      r
+    val s = available.take()
+    try f(s.allocateSocket)
     catch
-      case t: Throwable =>
-        doAndPrintError(s.close())
-        used.decrementAndGet()
+      case _: SocketException =>
+        s.invalidateSocket()
+        available.put(s)
         withSocket(f)
+    finally available.put(s)
 
   def close(): Unit =
     available.synchronized:
-      for s <- available do doAndPrintError(s.close())
+      for s <- available.asScala do s.invalidateSocket()
 
-  private def doAndPrintError[R](f: => Unit): Unit =
+  private def doAndPrintError(f: => Unit): Unit =
     try f
     catch case t: Throwable => t.printStackTrace()
+
 object SocketPool:
   def apply(host: String, port: Int, poolSz: Int = 32, retriesToOpenSocketBeforeGivingUp: Int = 128): SocketPool =
     val inetAddress = InetAddress.getByName(host)
