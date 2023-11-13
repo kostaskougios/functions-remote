1b5e4d876027d14978c294de099e2e7db7f38e6c
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-13 21:19:43 +0000

    -

--- end-to-end-tests/tests-loom-sockets/src/test/scala/endtoend/tests/loomsockets/LoomSocketsSuite.scala
+++ end-to-end-tests/tests-loom-sockets/src/test/scala/endtoend/tests/loomsockets/LoomSocketsSuite.scala
@@ -1,6 +1,6 @@
 package endtoend.tests.loomsockets
 
-import endtoend.tests.{SimpleFunctionsCallerFactory, SimpleFunctionsImpl, SimpleFunctionsReceiverFactory}
+import endtoend.tests.{SimpleFunctions, SimpleFunctionsCallerFactory, SimpleFunctionsImpl, SimpleFunctionsReceiverFactory}
 import functions.sockets.{FiberSocketServer, SocketPool, SocketTransport}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
@@ -10,42 +10,48 @@ import scala.concurrent.duration.*
 import scala.concurrent.{Await, Future}
 import concurrent.ExecutionContext.Implicits.global
 
-class LoomSocketsSuite extends AnyFunSuite with BeforeAndAfterAll:
-  val socketPool   = SocketPool("localhost", 7200)
-  val transport    = new SocketTransport(socketPool)
+class LoomSocketsSuite extends AnyFunSuite:
+
+  def withCallerDo(f: SimpleFunctions => Unit): Unit =
+    val socketPool = SocketPool("localhost", 7200)
+    val transport  = new SocketTransport(socketPool)
+    val caller     = SimpleFunctionsCallerFactory.newAvroSimpleFunctions(transport.transportFunction)
+    try f(caller)
+    finally socketPool.close()
+
   val invokerMap   = SimpleFunctionsReceiverFactory.invokerMap(new SimpleFunctionsImpl)
   def createServer = FiberSocketServer.withServer[Unit](7200, invokerMap) _
 
-  val caller = SimpleFunctionsCallerFactory.newAvroSimpleFunctions(transport.transportFunction)
-
-  override def afterAll(): Unit = socketPool.close()
-
   test("client/server") {
     createServer: server =>
-      caller.add(5, 6) should be(11)
+      withCallerDo: caller =>
+        caller.add(5, 6) should be(11)
   }
 
   test("concurrent requests") {
     createServer: server =>
-      val all = for i <- 1 to 10000 yield Future:
-        caller.add(i, 1) should be(i + 1)
+      withCallerDo: caller =>
+        val all = for i <- 1 to 10000 yield Future:
+          caller.add(i, 1) should be(i + 1)
 
-      // lets wait all to be ready to make sure the request completed.
-      // Then get the result to make sure we don't have an exception
-      for f <- all do Await.ready(f, 8.seconds)
-      for f <- all do Await.result(f, 1.seconds)
+        // lets wait all to be ready to make sure the request completed.
+        // Then get the result to make sure we don't have an exception
+        for f <- all do Await.ready(f, 8.seconds)
+        for f <- all do Await.result(f, 1.seconds)
   }
 
   test("client/server multiple requests") {
     createServer: server =>
-      caller.add(5, 6) should be(11)
-      caller.add(1, 2) should be(3)
-      caller.add(2, 3) should be(5)
+      withCallerDo: caller =>
+        caller.add(5, 6) should be(11)
+        caller.add(1, 2) should be(3)
+        caller.add(2, 3) should be(5)
   }
 
   test("request counter") {
     createServer: server =>
-      for i <- 1 to 10 do
-        caller.add(i, 1) should be(i + 1)
-        server.totalRequestCount should be(i)
+      withCallerDo: caller =>
+        for i <- 1 to 10 do
+          caller.add(i, 1) should be(i + 1)
+          server.totalRequestCount should be(i)
   }
