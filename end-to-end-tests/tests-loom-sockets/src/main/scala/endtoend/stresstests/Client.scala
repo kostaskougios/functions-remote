2b8e55e9383ba439a92a5abe39b45eb9e70bed09
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-13 12:35:54 +0000

    -

--- end-to-end-tests/tests-loom-sockets/src/main/scala/endtoend/stresstests/Client.scala
+++ end-to-end-tests/tests-loom-sockets/src/main/scala/endtoend/stresstests/Client.scala
@@ -9,4 +9,4 @@ import functions.sockets.SocketTransport
   for i <- 1 to 1_000_000 do
     val r = caller.add(i, i + 1)
     if r != i + i + 1 then throw new IllegalStateException(s"Server responded with wrong result at the ${i}th request.")
-    if i % 10000 == 0 then println(s"At the ${i}th request")
+    if i % 10000 == 0 then println(s"At the ${i}th request, activeThreads: ${ThreadCounter.countThreads()}")
