09f2aa9eeb412a33bf5aa36f78792eb0fc844b90
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-21 20:06:49 +0000

    -

--- end-to-end-tests/tests-helidon-server/src/test/scala/examples/ExampleServer.scala
+++ end-to-end-tests/tests-helidon-server/src/test/scala/examples/ExampleServer.scala
@@ -1,18 +1,14 @@
 package examples
 
-import io.helidon.config.Config
 import io.helidon.logging.common.LogConfig
 import io.helidon.webserver.WebServer
 import io.helidon.webserver.http.{HttpRouting, ServerRequest, ServerResponse}
 
 @main def exampleServer() =
   LogConfig.configureRuntime()
-  val config = Config.create
-  Config.global(config)
 
   def routing(routing: HttpRouting.Builder): Unit =
     routing.get("/simple-greet", (req: ServerRequest, res: ServerResponse) => res.send("Hello World!"))
 
-  val server = WebServer.builder.config(config.get("server")).routing(routing).build.start
-
+  val server = WebServer.builder.port(8080).routing(routing).build.start
   System.out.println("WEB server is up! http://localhost:" + server.port + "/simple-greet")
