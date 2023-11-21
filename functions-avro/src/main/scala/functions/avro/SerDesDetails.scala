b69084d5c89acd70f368eaf7791fa14c0fadc47d
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-21 12:28:52 +0000

    -

--- functions-avro/src/main/scala/functions/avro/SerDesDetails.scala
+++ functions-avro/src/main/scala/functions/avro/SerDesDetails.scala
@@ -8,10 +8,10 @@ class SerDesDetails[A](
     val decoder: Decoder[A]
 ):
   val avroOutputStreamBuilder: AvroOutputStreamBuilder[A] = outputStream[A](schemaFor, encoder)
-  val avroInputStreamBuilder: AvroInputStreamBuilder[A]   = inputStream[A](using decoder)
+  val avroInputStreamBuilder: AvroInputStreamBuilder[A]   = inputStream[A](decoder)
 
-  def inputStream[A](using decoder: Decoder[A]): AvroInputStreamBuilder[A] =
-    AvroInputStream.binary[A]
+  def inputStream[A](decoder: Decoder[A]): AvroInputStreamBuilder[A] =
+    AvroInputStream.binary[A](using decoder)
 
   def outputStream[A](schemaFor: SchemaFor[A], encoder: Encoder[A]): AvroOutputStreamBuilder[A] =
     AvroOutputStream.binary[A](using schemaFor, encoder)
