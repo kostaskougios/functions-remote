d135b082734e9f8a60b8ee441cf880d7dc69a877
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-22 13:20:00 +0000

    -

--- proxy-generator/src/main/resources/proxypackage/functionsreceiver/NoFrameworkImpl.scala
+++ proxy-generator/src/main/resources/proxypackage/functionsreceiver/NoFrameworkImpl.scala
@@ -11,4 +11,14 @@
     f.{{functionN}}{{^firstParamsRaw.isEmpty}}({{#firstParamsRaw}}paramsArgs.{{name}}{{^last}}, {{/last}}{{/firstParamsRaw}}){{/firstParamsRaw.isEmpty}}({{#paramsRaw}}params.{{name}}{{^last}}, {{/last}}{{/paramsRaw}})
     Array.emptyByteArray
     {{/isUnitReturnType}}
+
+  def {{functionN}}WithFrameworkParams{{firstParamsAndParens}}(in: ReceiverInput): Array[Byte] =
+    {{#paramsRaw.isEmpty}}if in.data.length>0 then throw new IllegalStateException(s"Expected empty array of bytes but got ${in.data.length}"){{/paramsRaw.isEmpty}}
+    {{^paramsRaw.isEmpty}}val params = {{functionN}}Deserializer(in.data) {{/paramsRaw.isEmpty}}
+    val r = f.{{functionN}}{{firstParamsCallAndParens}}({{#paramsRaw}}params.{{name}}{{^last}}, {{/last}}{{/paramsRaw}})
+    {{^isUnitReturnType}}
+    {{functionN}}ReturnTypeSerializer(r)
+    {{/isUnitReturnType}}
+    {{#isUnitReturnType}}Array.emptyByteArray{{/isUnitReturnType}}
+
   {{/mapResults}}
--- proxy-generator/src/main/resources/proxypackage/helidon/Routes.scala
+++ proxy-generator/src/main/resources/proxypackage/helidon/Routes.scala
@@ -13,7 +13,7 @@ class {{className}}(
   private val {{functionN}}Path = pathFor({{methodParams}}.Methods.{{caseClassName}})
   def {{functionN}}Route(req: ServerRequest, res: ServerResponse): Unit =
     val in = req.content().as(classOf[Array[Byte]])
-    val result = receiver.{{functionN}}(ReceiverInput(in))
+    val result = receiver.{{functionN}}WithFrameworkParams{{firstParamsCallAndParens}}(ReceiverInput(in))
     res.send(result)
 
   {{/functions}}
