08849185da15ac3e4bf0287176215d21907dfeed
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-22 19:23:51 +0000

    -

--- end-to-end-tests/tests-helidon-client/src/test/scala/examples/ExampleClient.scala
+++ end-to-end-tests/tests-helidon-client/src/test/scala/examples/ExampleClient.scala
@@ -2,7 +2,7 @@ package examples
 
 import endtoend.tests.helidon.TestsHelidonFunctionsCallerFactory
 import functions.helidon.transport.HelidonTransport
-import io.helidon.webclient.api.{ClientResponseTyped, WebClient}
+import io.helidon.webclient.api.WebClient
 
 // https://helidon.io/docs/v4/#/se/webclient
 @main def exampleClient(): Unit =
@@ -16,13 +16,3 @@ import io.helidon.webclient.api.{ClientResponseTyped, WebClient}
   val fJson     = TestsHelidonFunctionsCallerFactory.newJsonTestsHelidonFunctions(transport.transportFunction)
   println(fAvro.add(1, 2))
   println(fJson.add(10, 4))
-
-  def simpleGreetRoute(client: WebClient): ClientResponseTyped[Array[Byte]] = client.get.path("/simple-greet/Kos").request(classOf[Array[Byte]])
-  def testPostRoute(client: WebClient): ClientResponseTyped[Array[Byte]]    = client.post("/test-post").submit("Kostas".getBytes, classOf[Array[Byte]])
-
-  val getResponse  = simpleGreetRoute(client)
-  println(new String(getResponse.entity))
-  getResponse.close()
-  val postResponse = testPostRoute(client)
-  println(new String(postResponse.entity))
-  postResponse.close()
--- end-to-end-tests/tests-helidon-server/src/test/scala/examples/ExampleServer.scala
+++ end-to-end-tests/tests-helidon-server/src/test/scala/examples/ExampleServer.scala
@@ -8,24 +8,11 @@ import io.helidon.webserver.http.{HttpRouting, ServerRequest, ServerResponse}
 @main def exampleServer(): Unit =
   LogConfig.configureRuntime()
 
-  def testPost(req: ServerRequest, res: ServerResponse): Unit =
-    val in = req.content().as(classOf[Array[Byte]])
-    res.send(s"Hello ${new String(in)}!")
-
-  def testGet(req: ServerRequest, res: ServerResponse): Unit =
-    val name = req.path.pathParameters.get("name")
-    res.send(s"Hello $name!")
-
-  def routing(routing: HttpRouting.Builder): Unit =
-    routing.get("/simple-greet/{name}", testGet)
-    routing.post("/test-post", testPost)
-
   val impl       = new TestHelidonFunctionsImpl
   val avroRoutes = TestsHelidonFunctionsReceiverFactory.newAvroTestsHelidonFunctionsHelidonRoutes(impl)
   val jsonRoutes = TestsHelidonFunctionsReceiverFactory.newJsonTestsHelidonFunctionsHelidonRoutes(impl)
 
   val routeBuilder = HttpRouting.builder()
-  routing(routeBuilder)
   avroRoutes.routes(routeBuilder)
   jsonRoutes.routes(routeBuilder)
 
