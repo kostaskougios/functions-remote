83a2068debabb7c70bcbe02c27690ed0704cce96
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-23 17:59:39 +0000

    -

--- proxy-generator/src/main/resources/proxypackage/functionsreceiver/CatsFunctionImpl.scala
+++ proxy-generator/src/main/resources/proxypackage/functionsreceiver/CatsFunctionImpl.scala
@@ -1,10 +1,10 @@
   {{#mapResults}}
-  def {{functionN}}{{firstParamsAndParens}}(in: ReceiverInput): {{frameworkTypeArg}}[Array[Byte]] =
-    {{#paramsRaw.isEmpty}}if in.data.length>0 then throw new IllegalStateException(s"Expected empty array of bytes but got ${in.data.length}"){{/paramsRaw.isEmpty}}
-    {{^paramsRaw.isEmpty}}val params = {{functionN}}Deserializer(in.data) {{/paramsRaw.isEmpty}}
-    f.{{functionN}}{{firstParamsCallAndParens}}({{#paramsRaw}}params.{{name}}{{^last}}, {{/last}}{{/paramsRaw}}).map: r=>
+  def {{functionN}}{{firstParamsAndParens}}(in$: ReceiverInput): {{frameworkTypeArg}}[Array[Byte]] =
+    {{#paramsRaw.isEmpty}}if in$.data.length>0 then throw new IllegalStateException(s"Expected empty array of bytes but got ${in$.data.length}"){{/paramsRaw.isEmpty}}
+    {{^paramsRaw.isEmpty}}val params$ = {{functionN}}Deserializer(in$.data) {{/paramsRaw.isEmpty}}
+    f.{{functionN}}{{firstParamsCallAndParens}}({{#paramsRaw}}params$.{{name}}{{^last}}, {{/last}}{{/paramsRaw}}).map: r$=>
       {{^isUnitReturnType}}
-      {{functionN}}ReturnTypeSerializer(r)
+      {{functionN}}ReturnTypeSerializer(r$)
       {{/isUnitReturnType}}
       {{#isUnitReturnType}}Array.emptyByteArray{{/isUnitReturnType}}
   {{/mapResults}}
