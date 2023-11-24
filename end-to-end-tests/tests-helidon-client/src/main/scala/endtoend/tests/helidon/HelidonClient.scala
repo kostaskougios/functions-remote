28afb5e8828793a2ffaf08d35b1b4e983eb5bfbc
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-24 14:01:29 +0000

    -

--- end-to-end-tests/tests-helidon-client/src/main/scala/endtoend/tests/helidon/HelidonClient.scala
+++ end-to-end-tests/tests-helidon-client/src/main/scala/endtoend/tests/helidon/HelidonClient.scala
@@ -3,9 +3,8 @@ package endtoend.tests.helidon
 import io.helidon.webclient.api.WebClient
 
 object HelidonClient:
-  def newClient[R](serverPort: Int): WebClient =
+  def newClient(serverPort: Int): WebClient =
     WebClient
       .builder()
       .baseUri(s"http://localhost:$serverPort")
-      .keepAlive(true)
       .build()
